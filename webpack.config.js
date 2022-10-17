df5867e5dd52b17e04bba0f9e230a1c7a2c47938
Sulthan Nurya <53323434+shym2501@users.noreply.github.com>
2022-10-17 00:16:48 +0000

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,33 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js', },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Progressive Web Application',
+    main: './src/index.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Progressive Web App',
+      template: "./index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true
+      },
+      inject: false
     }),
-      new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-        clientsClaim: true,
-        skipWaiting: true,
-      }),
-    ],
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
-    },
+    new WorkboxPlugin.GenerateSW({
+      swSrc: "./src/src-sw.js",
+      swDest: "sw.js",
+      clientsClaim: true,
+      skipWaiting: true
+    }),
+  ],
+  output: {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
 };
